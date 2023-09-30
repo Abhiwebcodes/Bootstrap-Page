491eb96ba5b0913ca1261a4e75bcd15576eb373c
abhinnnvvv <125461645+Abhiwebcodes@users.noreply.github.com>
2023-09-30 18:05:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) ||2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+var options = {
+  accessibility: true,
+  prevNextButtons: true,
+  pageDots: true,
+  freeScroll: false,
+  wrapAround: true,
+  setGallerySize: false,
+
+  // arrowShape:
+  // 'M 10,30  A 20,20 0,0,1 50,30  A 20,20 0,0,1 90,30  Q 90,60 50,90  Q 10,60 10,30 z'
+  arrowShape: 'M 0,50 L 60,00 L 50,30 L 80,30 L 80,70 L 50,70 L 60,100 Z'
+}
+
+
+
+
+  
+// $('.navbar-btn').on('click', function() {
+//   $('.navbar-inner').toggleClass('in');
+// });
+
+
+  
+
+
+
+
+var carousel = document.querySelector('[data-carousel]');
+var slides = document.getElementsByClassName('carousel-cell');
+var flkty = new Flickity(carousel, options);
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="typewriter">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
